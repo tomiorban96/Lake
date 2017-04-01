42dae96639ce96ee13af7d6b1a68eaed6d408a88
cryengineer <cryengineer@gmail.com>
2017-04-01 23:19:51 +0200

    126

--- iskolabusz.cpp
+++ iskolabusz.cpp
@@ -2,12 +2,7 @@
 #include <cmath>
 int main()
 {
-	int i, N;
-	std::cin >> N;
-	if (N - 1)
-	{
-		i = log2(N - 1);
-		std::cout << (N - pow(2, i)) * 2;
-	}
+	int N; std::cin >> N;
+	if (N - 1) std::cout << (N - pow(2, trunc(log2(N - 1)))) * 2;
 	else std::cout << 1;
 }
